93049b7bcbc6c923fe4c28dd55bf43a273a89452
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-24 11:32:26 -0300

    test: add correct tags to InvestmentTests

--- src/test/java/br/ifsp/demo/domain/InvestmentTest.java
+++ src/test/java/br/ifsp/demo/domain/InvestmentTest.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InvestmentTest {
     @ParameterizedTest
+    @Tag("TDD")
+    @Tag("UnitTest")
     @CsvSource({"0,0,", "-1,1,", "-1,-1,"})
     @DisplayName("Should return error when try to create investment with invalid parameters")
     void shouldReturnErrorWhenTryToCreateInvestmentWithInvalidParameters(double initialValue, double recurrentValue, Asset asset){

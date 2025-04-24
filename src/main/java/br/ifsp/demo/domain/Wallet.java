4d905356514b08b71b81d1141fbf515d01e10e91
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-23 21:47:21 -0300

    refactor(wallet): improve method addInvestment to add on List of Investments

--- src/main/java/br/ifsp/demo/domain/Wallet.java
+++ src/main/java/br/ifsp/demo/domain/Wallet.java
@@ -1,7 +1,17 @@
 package br.ifsp.demo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet {
+    private final List<Investment> investments;
+
+    public Wallet() {
+        this.investments = new ArrayList<>();
+    }
+
     public boolean addInvestment(Investment investment) {
+        investments.add(investment);
         return true;
     }
 }

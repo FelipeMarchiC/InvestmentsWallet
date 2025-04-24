bb007d886a3f2958962d46f047c9b49d2eb9418b
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-24 11:10:36 -0300

    feat: implements the required values to create a new investment

--- src/main/java/br/ifsp/demo/domain/Investment.java
+++ src/main/java/br/ifsp/demo/domain/Investment.java
@@ -1,9 +1,13 @@
 package br.ifsp.demo.domain;
 
 public class Investment {
+    private final double initialValue;
+    private final double recurrentValue;
     private final Asset asset;
 
-    public Investment(Asset asset) {
+    public Investment(double initialValue, double recurrentValue, Asset asset) {
+        this.initialValue = initialValue;
+        this.recurrentValue = recurrentValue;
         this.asset = asset;
     }
 }

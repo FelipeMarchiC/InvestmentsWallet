d9619b5a517890e047666519e5225ee838c07982
Tiago Catoia <tiagocsz@hotmail.com>
2025-04-25 22:38:37 -0300

    refactor: add profitability field, validation and toString methods to Asset

--- src/main/java/br/ifsp/demo/domain/Asset.java
+++ src/main/java/br/ifsp/demo/domain/Asset.java
@@ -2,12 +2,29 @@ package br.ifsp.demo.domain;
 
 public class Asset {
     private final String name;
+    private final double profitability;
 
-    public Asset(String name) {
+    public Asset(String name, double profitability) {
+        verifyAsset(name, profitability);
         this.name = name;
+        this.profitability = profitability;
+    }
+
+    private void verifyAsset(String name, double profitability){
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Asset name cannot be null or blank");
+        if (profitability <= 0) throw new IllegalArgumentException("Asset profitability must be greater than zero");
+    }
+
+    @Override
+    public String toString() {
+        return "Asset name = " + name + " | Asset profitability = " + profitability;
     }
 
     public String getName() {
         return name;
+    }
+
+    public double getProfitability() {
+        return profitability;
     }
 }

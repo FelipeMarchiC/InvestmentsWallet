82805b2a3c8b94a38de01116a0620a52c2f1bb27
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-27 01:27:42 -0300

    test(InvestmentTest): improve invalid parameters test to test invalid parameters separated  - close #23

--- src/test/java/br/ifsp/demo/domain/InvestmentTest.java
+++ src/test/java/br/ifsp/demo/domain/InvestmentTest.java
@@ -4,22 +4,38 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InvestmentTest {
+
+    private static Stream<Arguments> getInvalidParameters() {
+        return Stream.of(
+                Arguments.of(-10, 50, new Asset("PETR4")),
+                Arguments.of(100, -10, new Asset("PETR4")),
+                Arguments.of(100, 50, null),
+                Arguments.of(0, 0, null)
+        );
+    }
+
     @ParameterizedTest
     @Tag("TDD")
     @Tag("UnitTest")
-    @CsvSource({"0,0,", "-1,1,", "-1,-1,"})
+    @MethodSource("getInvalidParameters")
     @DisplayName("Should return error when try to create investment with invalid parameters")
     void shouldReturnErrorWhenTryToCreateInvestmentWithInvalidParameters(double initialValue, double recurrentValue, Asset asset){
         assertThrows(IllegalArgumentException.class, () -> {
             Investment investment = new Investment(initialValue, recurrentValue, asset);
         });
     }
+
+
 
     @Test
     @Tag("TDD")

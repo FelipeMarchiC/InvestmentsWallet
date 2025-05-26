7902c44e6178dd83b06adab101eab7f61b627a21
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-05-26 11:13:58 -0300

    feat: create variables file to css. Change navbar color. Add wallet icon

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './Layout.css';
+import './Variables.css'
+import { IoWalletOutline } from "react-icons/io5";
 
 function Layout() {
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
+        <div id="left-side">
+          <IoWalletOutline size={30}/>
           <Link to="/" className="nav-logo">InvestmentsWallet</Link>
+        </div>
           <ul className="nav-links">
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
@@ -18,7 +23,7 @@ function Layout() {
         <Outlet />
       </main>
       <footer className="footer">
-        <p>&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} InvestmentsWallet. All rights reserved.</p>
       </footer>
     </>
   );

6a0410575df592af8c472e13793825db1e2ebd0e
FelipeMarchiC <felipe.marchi3004@gmail.co>
2025-06-22 17:25:09 -0300

    test: added suites for Api, Persistence, Integration and Ui tests

---
+++ backend/src/test/java/br/ifsp/demo/suite/ApiSuite.java
@@ -0,0 +1,12 @@
+package br.ifsp.demo.suite;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages({"br.ifsp.demo"})
+@SuiteDisplayName("All API Tests")
+@IncludeTags({"ApiTest"})
+public class ApiSuite { }
---
+++ backend/src/test/java/br/ifsp/demo/suite/UiSuite.java
@@ -0,0 +1,12 @@
+package br.ifsp.demo.suite;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages({"br.ifsp.demo"})
+@SuiteDisplayName("All UI Tests")
+@IncludeTags({"UiTest"})
+public class UiSuite { }
